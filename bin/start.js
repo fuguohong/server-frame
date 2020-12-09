706b75ac3e9cfd51ef9d0396f214e5e7b63e5f30
fuguohong <1347759962@qq.com>
2020-12-09 15:39:25 +0800

    test bin

---
+++ bin/start.js
@@ -0,0 +1,8 @@
+/**
+ * Created Date: 2020/12/9
+ * Author: fgh
+ * Description:
+ */
+'use strict'
+
+console.log('server start aaaaaaaaaaaaaaaaaaaaa')
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 /**
- * Created Date: 2020/12/9
+ * Created Date: 2020/10/15
  * Author: fgh
  * Description:
  */
 'use strict'
 
-module.exports = process.cwd()
+const fs = require('fs')
+const path = require('path')
+
+fs.readdirSync(__dirname)
+  .filter(f => f !== 'index.js')
+  .forEach(f => {
+    const name = f.slice(0, -3)
+    exports[name] = require(path.join(__dirname, f))
+  })
